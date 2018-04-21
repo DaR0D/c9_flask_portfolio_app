2b1b46ce437b39a63daae3a67ce289298efdedbd
DaR0D <rodrmanii222@gmail.com>
2018-04-20 17:38:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home_page():
 @app.route('/<name>')
 def profile(name):
 	newname = name + "come jamon"
-	return render_template('index.html', newname=name)
+	return render_template('index.html', name=newname)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
